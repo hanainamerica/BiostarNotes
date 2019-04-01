04e2453832ad75b2b211e9053e56dee86201493a
hanainamerica <hanainamerica@gmail.com>
2019-04-01 13:22:53 -0700

    add new project result

--- proj/data/2019-3-30/getdata.sh
+++ proj/data/2019-3-30/getdata.sh
@@ -3,3 +3,7 @@ curl http://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr22.fa.gz | gu
 
 # KU182908.fa
 efetch -id KU182908 -db nucleotide -format fasta > KU182908.fa
+
+# get all genomes from the ebola project
+esearch -db nuccore -query PRJNA257197 | efetch -format fasta > ebola.fa
+
